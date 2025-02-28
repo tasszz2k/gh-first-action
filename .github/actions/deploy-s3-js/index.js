2bcf279f043e6530b377987219f8abd4243449e8
Tass <tasszz2k@gmail.com>
2025-02-28 16:39:44 +0700

    section 9
    Signed-off-by: Tass <tasszz2k@gmail.com>

--- .github/actions/deploy-s3-js/index.js
+++ .github/actions/deploy-s3-js/index.js
@@ -4,6 +4,16 @@ const exec = require('@actions/exec');
 
 function run() {
     core.notice('Hello from my custom JS action');
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
+    const disFolder = core.getInput('dis-folder', {required: true});
+
+    // log inputs
+    core.notice(`bucket: ${bucket}`);
+    core.notice(`bucketRegion: ${bucketRegion}`);
+    core.notice(`disFolder: ${disFolder}`);
+
+    exec.exec('ls', ['-la']);
 }
 
 run();
